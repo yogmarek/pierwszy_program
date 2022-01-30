18de6d77efe08a6fd3f08522b226ed6f9715d786
yogmarek <97914252+yogmarek@users.noreply.github.com>
2022-01-30 17:13:16 +0100

    pierwszy_program
    Języki programowania

---
+++ pierwszy_program.py
@@ -0,0 +1,13 @@
+#pierwszy program w Python 
+#języki programowania
+name = input('Podaj nazwę jezyka programowania: ')
+if name == 'Python':
+    print('To jest język'' ' + name + '!')
+elif name =='PHP':
+    print('To jest język'' '  + name + '!')
+elif name == 'Java':
+    print('To jest język'' ' + name + '!')
+elif name == 'C++':
+    print ('To jest język'' '+ name + '!')
+else:
+    print('Nie znam takiego języka ! ')
